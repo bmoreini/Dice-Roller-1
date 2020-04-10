9215d3f48eb33235ac0055425e0e2b0f7cdf1732
bmoreini <bmoreini@bsdvt.org>
2020-04-10 18:10:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Code for madlibs.js
+var sides = document.getElementById("sides");
+var rollResult = document.getElementById("showResult");
+var dNum = document.getElementById("dNum");
+var showResult = document.getElementById("showResult");
+var diceBox = document.getElementById("diceBox");
+var sButton = document.getElementById("submit").addEventListener("click", rollDice);
+
+function rollDice(){
+  let faces = sides.value;
+  let dice = dNum.value;
+  let result = 0;
+  diceBox.style.backgroundSize = "100px 100px";
+  for ( let die =0; die<dice; die++){
+    showDice();
+    result+=(eachRoll(faces)); 
+    setTimeout(hideDice,3000);
+  }
+  rollResult.innerHTML = result;
+  console.log(result);
+}
+
+function eachRoll(faces){
+  showDice();
+  roll = Math.floor(Math.random()*+faces)+1;
+  return roll;
+}
+
+function showDice(){
+  diceBox.style.backgroundImage="url('dice.gif')";
+  showResult.classList.remove("showme");
+ //showResult.className += "hideme";
+}
+
+function hideDice(){
+  diceBox.style.backgroundImage="url('spacer.png')";
+  showResult.className += "showme";
+  //showResult.classList.remove("hideme");
+}
